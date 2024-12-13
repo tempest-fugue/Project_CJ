aa7cd07c0dd1276fe11ba78d7c147e9c2f5062aa
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2024-12-13 01:09:44 -0600

    add google maps library

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      {/* <APIProvider apiKey={AIzaSyBKsxB7SuZvOCJUCfWSsW9jkBRKtYBa0MY}>
+        <Map
+          style={{ width: "100vw", height: "100vh" }}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          defaultZoom={3}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        />
+      </APIProvider> */}
+    </div>
+  );
 }
 
 export default App;

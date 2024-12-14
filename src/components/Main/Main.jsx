3011d7734b2706b392f78feef40d245a7eb3f049
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2024-12-14 13:50:53 -0600

    add sections and add placeholder favicon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,12 +5,13 @@ import ellipse from "../../assets/Ellipse.svg";
 function Main() {
   return (
     <main>
-      Main
+      <section>About</section>
       <section className="section__map">
         <img className="section__map__image_top" src={ellipse}></img>
         <GoogleMap />
         <img className="section__map__image_bottom" src={ellipse}></img>
       </section>
+      <section>Cards/Events</section>
     </main>
   );
 }

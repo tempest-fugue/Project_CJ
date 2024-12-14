1a5a4b8dfcee9eb7ae5f607d03238679c7106027
Sam Branham <hotdogsb@gmail.com>
2024-12-14 17:07:30 -0600

    Add event section styling

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -5,12 +5,12 @@ function EventCard({ item }) {
     <li className="event-card">
       <img
         // onClick={handleCardClicked}
-        className="item-card__image"
+        className="event-card__image"
         src={item.imgLink}
         alt={item.name}
       />
       <div className="event-card__text-container">
-        <h2 className="event-card__title">{item.name}</h2>
+        <h3 className="event-card__title">{item.name}</h3>
         <p className="event-card__text">{item.description}</p>
       </div>
     </li>

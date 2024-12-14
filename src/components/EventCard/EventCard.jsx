bef99d4ed9936ba7507c2198bbfa491b9c907805
Sam Branham <hotdogsb@gmail.com>
2024-12-14 16:31:34 -0600

    Add event card markup and card data

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,7 +1,20 @@
 import "./EventCard.css";
 
-function EventCard() {
-  return <li className="event-card">Card</li>;
+function EventCard({ item }) {
+  return (
+    <li className="event-card">
+      <img
+        // onClick={handleCardClicked}
+        className="item-card__image"
+        src={item.imgLink}
+        alt={item.name}
+      />
+      <div className="event-card__text-container">
+        <h2 className="event-card__title">{item.name}</h2>
+        <p className="event-card__text">{item.description}</p>
+      </div>
+    </li>
+  );
 }
 
 export default EventCard;
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,14 +1,15 @@
 import "./Events.css";
 import EventCard from "../EventCard/EventCard";
+import { eventCardDatas } from "../../utils/constants";
 
 function Events() {
   return (
     <section className="events">
       <h2 className="events__title">Events</h2>
       <ul className="events__list">
-        <EventCard>1</EventCard>
-        <EventCard>2</EventCard>
-        <EventCard>3</EventCard>
+        {eventCardDatas.map((item, i) => {
+          return <EventCard key={i} item={item} />;
+        })}
       </ul>
     </section>
   );

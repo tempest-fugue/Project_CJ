93021ea91014c880a2fbab3d3765dfa652ee441d
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2024-12-13 23:49:58 -0600

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="app__content">
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;

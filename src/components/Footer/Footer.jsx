3479790d6f4a2a210574d70dd700515bc95507ef
davidbedoya <davidbedoya1000@gmail.com>
2024-12-15 00:35:14 -0500

    Project CJ Footer HTML and CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="footer__card"></div>
+        <div className="footer__card"></div>
+        <div className="footer__card"></div>
+        <div className="footer__card"></div>
+        <div className="footer__card"></div>
+      </div>
+      <p className="footer__copyright">
+        Copyright 2024 © - All Rights Reserved by Holiday Navigator
+      </p>
+    </div>
+  );
 }
 
 export default Footer;

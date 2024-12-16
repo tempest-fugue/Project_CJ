d1c3fa9587fc6042f950b9e27e0e4cb52dadf0df
Sam Branham <hotdogsb@gmail.com>
2024-12-16 00:57:31 -0600

    Update app.js

--- express_backend_project/app.js
+++ express_backend_project/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { spawn } = require("child_process");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, (error) => {
   if (!error)
